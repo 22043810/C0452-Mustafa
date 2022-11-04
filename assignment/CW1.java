89cc10945bf83de1b03e245a0613d8dba2319aa5
22043810 <22043810@bucks.ac.uk>
2022-11-04 17:00:13 +0000

    recently
    added

--- assignment/CW1.java
+++ assignment/CW1.java
@@ -30,20 +30,72 @@ public class Main {
         String opening = InputReader.getString ("\tWould you like to see a list of songs? yes/no ");
         opening.toLowerCase();
         if (opening.equals("y") || opening.equals("yes"))
+
 {
     for (Song song : songs)
-    { song.print() }
+    { song.print(); }
 
 }
+
+String views = InputReader.getString("\twould you like to see songs listed by its views? ");
+views.toLowerCase();
+if (views.equals("y")  || views.equals("yes"))
+
+{  
+    System.out.println("\t Songs Listed By Views: ");
+    PrintListByCount(songs);
+
+}
+
+String remove = InputReader.getString("\tto remove a song simply type the name of a song:");
+remove.toLowerCase();
+for (Song song : songs)
 
 {
-    String countlist = InputReader.getString("\twould you like to see songs listed by the views? yes/no");
-    
+
+    if(song.getname().toLowerCase().contains(remove))
+
+    {
+
+        System.out.println(remove + " " + song.getname());
+        songs.remove(song);
+        System.out.println(" song was found and successfully removed. ");
+        PrintList(songs);
+
+    }
+else
+System.out.println(" song could not be found.");
 }
+
     }
     
+    public static void printList(ArrayList<Song> songs)
+{
+    for (Song song : songs)
     
-    {
+    { song.print();}
+
+
         System.out.println("Mustafa's CW1");
     }
-}
+
+    public static void PrintListByCount(ArrayList<Song> localSongs)
+    {
+        int howLong = localSongs.size();
+        for (int highest= 0 ; highest< howLong; highgest++)
+
+        {
+            Song temporary = new Song("", ",0");
+            int highhestNumber=0;
+            for (Song song : localSongs)
+            {
+                if (highestNumber < song.getviews())
+                {
+                    highestNumber = song.getviews();
+                    temporary = song;
+                }
+            }
+            temporary.print();
+            localsongs.remove(temporary)
+        }
+    }
